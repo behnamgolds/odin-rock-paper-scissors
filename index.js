0129c616ab3140c141e8029de621540dfcc21a88
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2023-07-01 09:49:40 +0330

    Add play function
    The play function compares playerChoice with computerChoice and returns
    a string to notify the user about a win, lose or tie.

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function getComputerChoice(){
 }
 
 function play(playerChoice, computerChoice){
-    
+    if(playerChoice === computerChoice){
+        return "It's a tie."
+    }else if((playerChoice === "rock" && computerChoice === "scissors")
+    || (playerChoice === "paper" && computerChoice === "rock")
+    || (playerChoice === "scissors" && computerChoice === "paper")){
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    } else {
+        return `You Lose! ${computerChoice} beats ${playerChoice}`;
+    }
 }
