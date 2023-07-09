a0170ff2f6a44d1d75813b0e9896faa8d319b240
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2023-07-09 16:27:12 +0330

    Add wins section and reset button
    Once a player reaches a point of 5 the game is ended and the buttons are
    diabled. With the reset button we can reset the game at any point.

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ function getComputerChoice() {
 
 function play(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
+    tiesCount++;
     return "It's a tie.";
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
+    pwinCount++;
     return `You Win! ${playerChoice} beats ${computerChoice}`;
   } else {
+    cwinCount++;
     return `You Lose! ${computerChoice} beats ${playerChoice}`;
   }
 }
@@ -27,13 +30,51 @@ function game(e) {
     result = play(playerChoice, computerChoice);
     // console.log(result);
   }
+  if (pwinCount === 5) {
+    toggleButtons();
+    result = "You Won The Game!";
+  } else if (cwinCount === 5) {
+    toggleButtons();
+    result = "You Lost The Game!";
+  }
   message.innerText = result;
-  pchoice.innerText = playerChoice;
-  cchoice.innerText = computerChoice;
+  pchoice.innerText = this.innerText;
+  cchoice.innerText = computerChoice[0].toUpperCase() + computerChoice.slice(1);
+  pwin.innerText = pwinCount;
+  cwin.innerText = cwinCount;
+  ties.innerText = tiesCount;
+}
+
+function toggleButtons() {
+  buttons.forEach((button) => button.toggleAttribute("disabled"));
+}
+
+function resetGame(e) {
+  pwin.innerText = 0;
+  cwin.innerText = 0;
+  ties.innerText = 0;
+  pwinCount = 0;
+  cwinCount = 0;
+  tiesCount = 0;
+  message.innerText = "Press a button";
+  if (buttons[0].disabled) {
+    toggleButtons();
+  }
 }
 
 const buttons = document.querySelectorAll(".game-buttons > button");
 buttons.forEach((button) => button.addEventListener("click", game));
+
 const message = document.querySelector(".message");
+
 const pchoice = document.querySelector("#pchoice");
 const cchoice = document.querySelector("#cchoice");
+
+const pwin = document.querySelector("#pwin");
+const cwin = document.querySelector("#cwin");
+const ties = document.querySelector("#ties");
+
+document.querySelector(".reset").addEventListener("click", resetGame);
+let pwinCount = 0;
+let cwinCount = 0;
+let tiesCount = 0;
