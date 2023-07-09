b776c1e4c6c9daba927dde81e2cdcc3e6d78a458
behnamgolds <29102609+behnamgolds@users.noreply.github.com>
2023-07-09 13:30:00 +0330

    Remove while loop and addEventListener to buttons

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const gameChoices = ['rock', 'paper', 'scissors'];
+const gameChoices = ["rock", "paper", "scissors"];
 const quitChoices = ["quit", "bye", "exit", "end"];
- 
-function getComputerChoice(){
-    return gameChoices[Math.floor(Math.random() * 3)];
+
+function getComputerChoice() {
+  return gameChoices[Math.floor(Math.random() * 3)];
 }
 
-function play(playerChoice, computerChoice){
-    if(playerChoice === computerChoice){
-        return "It's a tie."
-    }else if((playerChoice === "rock" && computerChoice === "scissors")
-    || (playerChoice === "paper" && computerChoice === "rock")
-    || (playerChoice === "scissors" && computerChoice === "paper")){
-        return `You Win! ${playerChoice} beats ${computerChoice}`;
-    } else {
-        return `You Lose! ${computerChoice} beats ${playerChoice}`;
-    }
+function play(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "It's a tie.";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    return `You Win! ${playerChoice} beats ${computerChoice}`;
+  } else {
+    return `You Lose! ${computerChoice} beats ${playerChoice}`;
+  }
 }
 
-function game(){
-    let continuePlaying = true;
-    while(continuePlaying){
-        let playerChoice = prompt("Enter choice:").toLowerCase();
-        if(gameChoices.includes(playerChoice)){
-            let computerChoice = getComputerChoice();
-            let result = play(playerChoice, computerChoice);
-            console.log(result);
-        } else if(quitChoices.includes(playerChoice)) {
-            console.log("You quit the game.");
-            continuePlaying = false;
-        } else {
-            console.log("Please select between 'rock', 'paper', 'scissors'");
-        }
-    }
+function game(e) {
+  const playerChoice = e.target.classList.value;
+  if (gameChoices.includes(playerChoice)) {
+    let computerChoice = getComputerChoice();
+    let result = play(playerChoice, computerChoice);
+    console.log(result);
+  } else if (quitChoices.includes(playerChoice)) {
+    console.log("You quit the game.");
+    continuePlaying = false;
+  } else {
+    console.log("Please select between 'rock', 'paper', 'scissors'");
+  }
 }
+
+const buttons = document.querySelectorAll(".game-buttons > button");
+buttons.forEach((button) => button.addEventListener("click", game));
